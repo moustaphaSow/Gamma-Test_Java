160cc965b9663cc9305cd78eee948910712d0b79
Mouhamadou Moustapha Sow <65513288+moustaphaSow@users.noreply.github.com>
2023-07-18 16:06:19 +0200

    Update MainTest.java

--- src/MainTest.java
+++ src/MainTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void main() {
+    void Test() {
         // Divisible par 3 et 5
         assertEquals("FizzBuzz", "15");
         // Divisible par 3
